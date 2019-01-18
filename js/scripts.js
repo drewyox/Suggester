869dc211861b84f9723be73dedd0b5b60bc7aac2
Andrew Yoxsimer <Yox@Andrews-MacBook-Air.local>
2019-01-18 12:39:10 -0800

    add JS functionality, hiding questions, displaying answers, and moving to next question.

---
+++ js/scripts.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+
+  $("#question0").submit(function(event) {
+      event.preventDefault();
+      var ready = $("input:radio[name=yesNoOne]:checked").val();
+
+      if (ready === "yes") {
+        $("#question0Content").hide();
+        $("#question1Content").show();
+      }
+      else {
+        $("#question0Content").hide();
+        $(".infoLink").show();
+      };
+    });
+
+    $("#questionOne").submit(function(event) {
+      event.preventDefault();
+      var qstOne = $("input:radio[name=yesNoOne1]:checked").val();
+      if (qstOne === "yes") {
+        $("#question1content").hide();
+        $("#question0Content").hide();
+        $(".cssInfo").show();
+      }
+      else {
+        $("#question1Content").hide();
+        $(".cssInfo").hide();
+        $("#question2Content").show();
+      };
+    });
+
+    $("#questionTwo").submit(function(event) {
+      event.preventDefault();
+      var qstTwo = $("input:radio[name=yesNoOne2]:checked").val();
+      if (qstTwo === "yes") {
+        $("#question2content").hide();
+        $("#question1Content").hide();
+        $(".rubyInfo").show();
+      }
+      else {
+        $("#question2Content").hide();
+        $(".rubyInfo").hide();
+        $("#question3Content").show();
+      };
+    });
+
+    $("#questionThree").submit(function(event) {
+      event.preventDefault();
+      var qst3 = $("input:radio[name=yesNoOne3]:checked").val();
+      if (qst3 === "yes") {
+        $("#question3content").hide();
+        $("#question2Content").hide();
+        $(".cshInfo").show();
+      }
+      else {
+        $("#question3Content").hide();
+        $(".cshInfo").hide();
+        $("#question4Content").show();
+      };
+    });
+
+    $("#questionFour").submit(function(event) {
+      event.preventDefault();
+      var qst4 = $("input:radio[name=yesNoOne4]:checked").val();
+      if (qst4 === "yes") {
+        $("#question4content").hide();
+        $("#question3Content").hide();
+        $(".javaInfo").show();
+      }
+      else {
+        $(".javaInfo").hide();
+        $(".phpInfo").show();
+      };
+    });
+
+
+  });
